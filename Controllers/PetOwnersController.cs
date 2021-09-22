0d9f3ee31bb78e8c9d71948586d376ef9eda2c1f
Benjamin Henke <benjaminmhenke@gmail.com>
2021-09-22 16:42:37 -0500

    Working on GETS

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -19,9 +19,9 @@ namespace pet_hotel.Controllers
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() 
+        public IEnumerable<PetOwner> GetList() 
         {
-            return new List<PetOwner>();
+            return _context.PetOwner;
         }
 
         [HttpPost]
